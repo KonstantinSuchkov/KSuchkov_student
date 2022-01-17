8ca54d690494bbc75361e0ca52bd53634817db37
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2022-01-17 19:46:19 +0300

    Revert "моя четвертая домашняя работа на GitHub"
    This reverts commit 15b11826b7447c0036c8bf2b52c2315e86f7f68a.

--- hw4_py/k_suchkov_4_4.py
+++
@@ -1,7 +0,0 @@
-import utils
-
-print(utils.currency_rates('USD'))
-print(utils.currency_rates('EUR'))
-print(utils.currency_rates('USD') + utils.currency_rates('EUR'))
-print(utils.currency_rates('USD') * 1500)
-
