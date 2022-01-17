db61ee3bc2e62bfb1ee7e98b81713588f9eb90fc
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2022-01-17 19:46:15 +0300

    Revert "моя четвертая домашняя работа на GitHub"
    This reverts commit 2883b188bb55182050c127f52aa1c5baf64a3587.

--- hw4_py/k_suchkov_4_2.py
+++ hw4_py/k_suchkov_4_2.py
@@ -1,9 +1,8 @@
 # 2. Написать функцию currency_rates(), принимающую в качестве аргумента код валюты (например, USD, EUR, GBP, ...)
 # и возвращающую курс этой валюты по отношению к рублю. Использовать библиотеку requests
 from requests import get
-from decimal import Decimal  # в задании буду использовать Decimal, тк иначе значение функция возвращала в
-
-# виде str, а преобразовать в float у меня не получилось (valueerror could not convert string to float python)
+from decimal import Decimal # в задании буду использовать Decimal, тк иначе значение функция возвращала в
+# виде str, а преобразовать в float меня не получилось (valueerror could not convert string to float python)
 #
 
 response = get('http://www.cbr.ru/scripts/XML_daily.asp')
@@ -27,5 +26,6 @@ def currency_rates(valute):
 print(currency_rates('USD'))
 print(type(currency_rates('USD')))
 print(currency_rates('EUR'))
-print(currency_rates('SOMEVALUTE'))  # проверяем на несуществующую валюту, ответ None
-print(currency_rates('USD') + currency_rates('EUR'))  # проверка на математику
+print(currency_rates('SOMEVALUTE')) # проверяем на несуществующую валюту, ответ None
+print(currency_rates('USD') + currency_rates('EUR')) # проверка на математику
+
--- hw4_py/k_suchkov_4_4.py
+++ hw4_py/k_suchkov_4_4.py
@@ -4,3 +4,4 @@ print(utils.currency_rates('USD'))
 print(utils.currency_rates('EUR'))
 print(utils.currency_rates('USD') + utils.currency_rates('EUR'))
 print(utils.currency_rates('USD') * 1500)
+
